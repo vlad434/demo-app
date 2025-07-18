f1f4e57536a6df43f4f2bcc73f77af21a64fd311
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2025-07-18 10:13:43 +0300

    Refactor price validation in product schema to use z.coerce for number conversion

--- src/features/products/schema/schema.ts
+++ src/features/products/schema/schema.ts
@@ -2,13 +2,7 @@ import { z } from "zod";
 
 export const productSchema = z.object({
   name: z.string().min(1, "Name is required"),
-  // price: z.coerce.number().min(0, "Price has to be min. 1"),
-  price: z
-    .string()
-    .min(1, "Price is required")
-    // .refine((val) => !isNaN(Number(val)), "Price must be a number")
-    .transform((val) => Number(val))
-    .pipe(z.number().min(0, "Price must be at least 0")),
+  price: z.coerce.number().min(1, "Price has to be min. 1"),
   description: z.string().optional(),
 });
 
